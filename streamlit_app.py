ffaadf53d149e4e1c61eb6ecab74345c1cb9bad0
django <108687989+j-isaac-melle@users.noreply.github.com>
2022-07-04 15:13:47 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -12,4 +12,10 @@ streamlit.header('🍍🍌Build your own fruit smoothie')
 import pandas
 
 my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
+
+#pick select list here for smothie creation: 
+streamlit.multiselect(“pick some fruits:”, list(my_fruit_list.index))
+
+#display table on page 
 streamlit.dataframe(my_fruit_list)
+
