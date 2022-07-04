9642bfe768e34f7e61b931fee3619427093b0b42
django <108687989+j-isaac-melle@users.noreply.github.com>
2022-07-04 14:39:36 -0400

    Update streamlit_app.py
    Added headers and text onto lunch menu

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,3 +1,9 @@
 import streamlit 
 
 streamlit.title('Resto Melle')
+
+streamlit.header('Lunch Menu')
+streamlit.text('BLT/Avo Sandwhich')
+streamlit.header('Chicken & Waffles')
+streamlit.header('Twista Salad')
+streamlit.header('Spaghetti Pizza')
