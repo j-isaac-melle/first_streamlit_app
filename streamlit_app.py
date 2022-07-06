af61cb7723e21fa243a6ef0c8aff6c50889bb9d2
django <108687989+j-isaac-melle@users.noreply.github.com>
2022-07-06 06:23:59 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -20,7 +20,7 @@ my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.co
 my_fruit_list = my_fruit_list.set_index('Fruit')
 
 #pick select list here for smothie creation: 
-streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index))
+streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index)),['Avocado','Strawberry'])
 
 #display table on page 
 streamlit.dataframe(my_fruit_list)
