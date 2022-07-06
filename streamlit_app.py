14931c6f9a3444b8474d345971fab9c073ea8bbf
django <108687989+j-isaac-melle@users.noreply.github.com>
2022-07-06 06:19:02 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -9,10 +9,14 @@ streamlit.text('🌪Twista Salad')
 streamlit.text('🍕Spaghetti Pizza')
 streamlit.header('🍍🍌Build your own fruit smoothie')
 
+#import python pandas package 
 import pandas
 
+#object my_fruit_list 
+# CSV must be read by pandas from the S3 bucket, use read_csv to pull the data into dataframe 
 my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
 
+#sets the index to Fruit column NOT NUMBERS
 my_fruit_list = my_fruit_list.set_index('Fruit')
 
 #pick select list here for smothie creation: 
