57bc355c2a8f7fca3e695fc2827290fd39941949
django <108687989+j-isaac-melle@users.noreply.github.com>
2022-07-07 06:01:36 -0400

    Update streamlit_app.py
    added comments, took away large table.

--- streamlit_app.py
+++ streamlit_app.py
@@ -20,7 +20,9 @@ my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.co
 my_fruit_list = my_fruit_list.set_index('Fruit')
 
 #pick select list here for smothie creation: (list from index index declared)
+#manually adds avo and bana to list 
 fruits_selected = streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index),['Avocado','Strawberries'])
+#hides the giant table 
 fruits_to_show = my_fruit_list.loc[fruits_selected]
 
 #display table on page (was my fruit list) now just fruits to show 
