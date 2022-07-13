88cfbf3c0884ef95d9789f328590809a09b680e3
django <108687989+j-isaac-melle@users.noreply.github.com>
2022-07-13 10:33:32 -0400

    Update streamlit_app.py
    brought new python package on

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,30 +1,27 @@
 import streamlit 
 
 streamlit.title('Resto Melle')
-
 streamlit.header('Lunch Menu')
 streamlit.text('🥑BLT/Avo Sandwhich')
 streamlit.text('🧇Chicken & Waffles')
 streamlit.text('🌪Twista Salad')
 streamlit.text('🍕Spaghetti Pizza')
 streamlit.header('🍍🍌Build your own fruit smoothie')
-
 #import python pandas package 
 import pandas
-
 #object my_fruit_list 
 # CSV must be read by pandas from the S3 bucket, use read_csv to pull the data into dataframe 
 my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
-
 #sets the index to Fruit column NOT NUMBERS
 my_fruit_list = my_fruit_list.set_index('Fruit')
-
 #pick select list here for smothie creation: (list from index index declared)
 #manually adds avo and bana to list 
 fruits_selected = streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index),['Avocado','Strawberries'])
 #hides the giant table 
 fruits_to_show = my_fruit_list.loc[fruits_selected]
-
 #display table on page (was my fruit list) now just fruits to show 
 streamlit.dataframe(fruits_to_show)
 
+import requests
+fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
+streamlit.text(fruityvice_response)
