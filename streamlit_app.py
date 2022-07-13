82c0451f350a9b4b3a4d7a5760b6f8512486b977
django <108687989+j-isaac-melle@users.noreply.github.com>
2022-07-13 10:43:11 -0400

    Update streamlit_app.py
    # new section to display fruity api response

--- streamlit_app.py
+++ streamlit_app.py
@@ -21,6 +21,8 @@ fruits_selected = streamlit.multiselect("Pick some fruits:", list(my_fruit_list.
 fruits_to_show = my_fruit_list.loc[fruits_selected]
 #display table on page (was my fruit list) now just fruits to show 
 streamlit.dataframe(fruits_to_show)
+#new section to display fruityvice api response
+streamlit.header("Fruityvice Fruit Advice!")
 
 import requests
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
