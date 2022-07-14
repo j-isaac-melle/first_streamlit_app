fb10679239c49da55ed652186ad4fd03979b4408
django <108687989+j-isaac-melle@users.noreply.github.com>
2022-07-14 11:15:41 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -38,26 +38,30 @@ try:
   if not fruit_choice:
     streamlit.error("please select a fruit to get information.")
   else:
-      #import requests
       back_from_function = get_fruityvice_data(fruit_choice)
-      #build new table for genus 
       streamlit.dataframe(back_from_function)
 
 except URLError as e:
     streamlit.error()
+
+streamlit.header("The fruit load list contains:")
+#Snowflake-related functions
+def get_fruit_load_list();
+      with my_cnx.cursor() as my_cur:
+           my_cur.execute("SELECT * from fruit_load_list")
+      return my_cur.fetchall()
+
+# add a button to load fruit
+if streamlit.button('Get Fruit Load List'):
+      my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
+      my_data_rows = get_fruit_load_list()
+      streamlit.dataframe(my_data_rows)
 #connecting to snowflake
 #dont run anything past while troubleshooting:
 streamlit.stop()
 
 
 #import snowflake.connector
-
-my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
-my_cur = my_cnx.cursor()
-my_cur.execute("SELECT * from fruit_load_list")
-my_data_rows = my_cur.fetchall()
-streamlit.header("The fruit load list contains:")
-streamlit.dataframe(my_data_rows)
 
 add_my_fruit = streamlit.text_input('What fruit would you like to add?','jackfruit')
 streamlit.write('Thanks for adding ', add_my_fruit)
