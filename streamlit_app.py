f2e965d69d38546b771056b0a435ec61108c7587
django <108687989+j-isaac-melle@users.noreply.github.com>
2022-07-14 14:10:07 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -53,18 +53,30 @@ def get_fruit_load_list():
 
 # add a button to load fruit
 if streamlit.button('Get Fruit Load List'):
-      my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
+      
       my_data_rows = get_fruit_load_list()
       streamlit.dataframe(my_data_rows)
 #connecting to snowflake
+#allow the end user to add a fruit to the list 
+def insert_row_snowflake(new_fruit):
+     with my_cnx.cursor() as my_cur:
+            my_cur.execute("insert into fruit_load_list values ('from streamlit')")
+            return "Thanks for adding ", add_my_fruit
+
+add_my_fruit = streamlit.text_input('What fruit would you like to add?')
+if streamlit.button('Add a Fruit to the list'):
+          my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
+           back_from_function = insert_row_snowflake(add_my_fruit)
+            streamlit.text(back_from_function)
+     
 #dont run anything past while troubleshooting:
 streamlit.stop()
 
 
 #import snowflake.connector
 
-add_my_fruit = streamlit.text_input('What fruit would you like to add?','jackfruit')
-streamlit.write('Thanks for adding ', add_my_fruit)
+add_my_fruit = _input('What fruit would you like to add?','jackfruit')
+streamlit.write()
 
 # this will not work but go wiith it for now
-my_cur.execute("insert into fruit_load_list values ('from streamlit')")
+
